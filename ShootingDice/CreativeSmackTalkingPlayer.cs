0d3b84f1a7fbb966aa4934a4b4c148efb08cc5dc
Meg McGregor <megmcgregor1@gmail.com>
2021-06-08 14:00:28 -0700

    checking out for the day

--- ShootingDice/CreativeSmackTalkingPlayer.cs
+++ ShootingDice/CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,26 @@
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        List<string> taunts = new List<string>
+            {
+                "You are proof that God has a sense of humor.",
+                "You have delusions of adequecy",
+                "I like the way you try",
+                "I like your approach, now let's see your departure",
+                "It's impossible to underestimate you"
+            };
+
+
+        public override void Play(Player other)
+        {
+
+            base.Play(other);
+        }
+
 
     }
 }
